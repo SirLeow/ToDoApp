dec76049be44e15d2c325482915e8de53e5c7cf7
Thiago Barros <t.b.lima18@gmail.com>
2022-03-07 19:00:35 -0300

    Fixed the time text display

--- app/src/main/java/com/example/todoapp/hour/TimeManager.kt
+++ app/src/main/java/com/example/todoapp/hour/TimeManager.kt
@@ -1,6 +1,7 @@
 package com.example.todoapp.hour
 
 import androidx.fragment.app.FragmentManager
+import com.example.todoapp.activity.time
 import com.example.todoapp.databinding.ActivityAddBinding
 import com.google.android.material.timepicker.MaterialTimePicker
 import com.google.android.material.timepicker.TimeFormat
@@ -19,6 +20,11 @@ class TimeManager {
     }
 
     private fun format(time: MaterialTimePicker):String{
+        when{
+            time.hour <10 && time.minute <10 -> return "0${time.hour}:0${time.minute}"
+            time.hour <10 -> return "0${time.hour}:${time.minute}"
+            time.minute <10 -> return "${time.hour}:0${time.minute}"
+        }
         return "${time.hour}:${time.minute}"
     }
 }
